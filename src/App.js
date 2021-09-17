5f8c97cd3b1538afd4aa40a32c851cf27f1270c7
Travis Hughes <travis.a.hughes@gmail.com>
2021-09-17 01:41:08 -0400

    aws amplify setup. ethers install....

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import { ethers } from 'ethers';
 import './App.css';
 
 function App() {
+  if (typeof window.ethereum !== 'undefined') {
+    console.log('Eth found!')
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    console.log('provider', provider)
+  } else {
+    console.log('No eth found')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello from V3</h1>
       </header>
     </div>
   );
