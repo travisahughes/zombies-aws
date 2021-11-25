dc955a92672e2b03f6e9c2c8a18123d632020932
Kevin Cane <kevincane.engineer0209@gmail.com>
2021-11-24 21:50:31 -0500

    finish info section animation

--- src/pages/Challenge/Info.js
+++ src/pages/Challenge/Info.js
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react';
 import gsap from 'gsap';
 import zombie from '../../assets/challenge/info-zombie.png';
 import background from '../../assets/challenge/info-background.png';
+import pigeon from '../../assets/pigeon.png';
+
 import './Info.css';
 
 const maxes = [];
@@ -49,12 +51,68 @@ function Info({ userNfts }) {
       delay: 5,
       ease: 'power1.inOut',
     });
+    gsap.to('.next-step-button', {
+      duration: 0.5,
+      opacity: 100,
+      delay: 5,
+      ease: 'power1.inOut',
+    });
+  };
+
+  const handleNext = () => {
+    gsap.to('.next-step-button', {
+      duration: 0.5,
+      opacity: 0,
+      ease: 'power1.easeOut',
+    });
+    gsap.to('.zombie-image', {
+      duration: 0.5,
+      opacity: 0,
+      ease: 'power1.easeOut',
+    });
+    gsap.to('.info-page-background', {
+      duration: 0.5,
+      opacity: 0,
+      ease: 'power1.easeOut',
+    });
+    gsap.to('.your-challenge-text', {
+      duration: 0.5,
+      opacity: 0,
+      ease: 'power1.easeOut',
+    });
+    gsap.to('.pigeon-image', {
+      duration: 0.5,
+      opacity: 1,
+      ease: 'power1.inOut',
+      delay: 0.5,
+    });
+
+    gsap.to('.pigeon-image', {
+      duration: 0.5,
+      opacity: 0,
+      ease: 'power1.inOut',
+      delay: 6,
+    });
+
+    gsap.to('.pigeon-text', {
+      duration: 0.5,
+      opacity: 1,
+      ease: 'power1.inOut',
+      delay: 6,
+    });
   };
   return (
     <div className="info-page">
       <div className="zombie-image">
         <img src={zombie} alt="" />
         <div className="welcome-text">Welcome!</div>
+      </div>
+      <div className="pigeon-image">
+        <img src={pigeon} alt="" />
+        <div className="pigeon-image-text">
+          Get your team together to pull off the heist and obtain your reward
+          before time runs out!
+        </div>
       </div>
       <div className="info-page-background">
         <img src={background} alt="" />
@@ -63,6 +121,13 @@ function Info({ userNfts }) {
         Your challenge (should you choose to accept it) is to assemble a team of
         NFZs with a very particular set of skills.
       </div>
+      <div className="pigeon-text">
+        To pull off this heist, you will need three NFZs with the following
+        skills.
+      </div>
+      <button className="next-step-button" onClick={() => handleNext()}>
+        Next
+      </button>
     </div>
   );
 }
