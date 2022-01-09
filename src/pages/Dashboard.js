6978d2a9aebd30b7af271daec394e6c27788fd5a
Travis Hughes <bl00dl1n3@gmail.com>
2022-01-08 22:54:01 -0500

    Updating dao est value

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -747,7 +747,7 @@ function Dashboard() {
             <h2>NFZ School DAO</h2>
             <div id="dao-value">
               Current Estimate Value:{' '}
-              <span className="green-highlight">48ETH</span>
+              <span className="green-highlight">44ETH</span>
               <br />
               <span className="subtext">(Updated weekly)</span>
             </div>
