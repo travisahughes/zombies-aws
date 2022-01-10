c43aa4ad592402b86a6f03302873767712af13d1
jianesis <davidjian0905@gmail.com>
2022-01-10 10:02:42 +0800

    update with more links

--- src/Components/QuickAccess.jsx
+++ src/Components/QuickAccess.jsx
@@ -6,6 +6,7 @@ import keycard from '../assets/locations/keycard.png';
 import graveBg from '../assets/bg-grave.jpg';
 import splitBg from '../assets/bg-splitpath.jpg';
 import styled from '@emotion/styled';
+import { useHistory } from 'react-router-dom';
 
 const QuickAccessSection = styled.section`
   display: flex;
@@ -29,6 +30,7 @@ const DashboardAccess = styled.div`
   background-repeat: no-repeat;
   background-position-x: center;
   background-size: cover;
+  cursor: pointer;
 `;
 
 const LocationAccess = styled.div`
@@ -45,6 +47,7 @@ const LocationAccess = styled.div`
   background-position-x: center;
   background-size: cover;
   padding: 16px;
+  cursor: pointer;
 `;
 
 const TextContainer = styled.div`
@@ -89,9 +92,10 @@ const SchoolImage = styled.img`
 `;
 
 export default function QuickAccess() {
+  const history = useHistory();
   return (
     <QuickAccessSection>
-      <DashboardAccess>
+      <DashboardAccess onClick={() => history.push('/dashboard')}>
         <KeycardImage src={keycard} alt="keycard" />
         <TextContainer>
           <Title>OWNER DASHBOARD</Title>
@@ -101,7 +105,7 @@ export default function QuickAccess() {
           </p>
         </TextContainer>
       </DashboardAccess>
-      <LocationAccess>
+      <LocationAccess onClick={() => history.push('/locations')}>
         <CasinoImage src={casino} alt="casino" />
         <SchoolImage src={school} alt="school" height={196} />
         <TextContainer>
